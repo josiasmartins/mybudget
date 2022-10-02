91c7db1d9b2124a8d7adf5ad19b6d20da611c570
Caitano <josiasmartinscaitano@gmail.com>
2022-10-02 10:43:21 -0300

    wip: sedebar

--- myindex.py
+++ myindex.py
@@ -6,6 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
+from components import sidebar, dashboards, extratos
 
 
 
@@ -15,15 +16,30 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
-
-
-
-
-
+    # dbc.Row: linha
+    dbc.Row([
+        # dbc.Col: coluna
+        dbc.Col([
+            dcc.Location(id='url'),
+            # instanciar novo component 
+            sidebar.layout
+        ], md=2),
+        dbc.Col([
+            content
+        ], md=10)
+    ])
+# fluid: responsivo
 ], fluid=True,)
 
 
+# colback
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def render_page(pathname): 
+    if pathname == '/' or pathname == '/dashboard': 
+        return dashboards.layout
 
+    if pathname == '/extratos':
+        return extratos.layout
 
 if __name__ == '__main__':
     app.run_server(port=8051, debug=True)
